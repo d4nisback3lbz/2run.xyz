395eda90e2bdab68ebf70a0c6385421957584757
d4nisback3lbz <sigmermaxing29@proton.me>
2024-10-15 13:44:00 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 local ids = {2569453732}
 
 for _, v in ipairs(ids) do
-    
+    if v == game.PlaceId then
+        local url = "https://github.com/d4nisback3lbz/2run.xyz/blob/main/%s.lua".format(v.tostring())
+        loadstring(game:HttpGet(url))()
+    end
 end
